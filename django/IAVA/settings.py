88e8537684b8f1a0ea4df4ac9530f5bf99b99b07
John Carlo <mikejayoma@gmail.com>
2026-09-08 20:20:40 +0800

    Added requirements.txt for DjangoI

--- django/IAVA/settings.py
+++ django/IAVA/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,13 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cwxn(f*6kos=m1ir$6u8+@ke3ygt@5*fqb)i(b9n(-dtsc_r#9'
+SECRET_KEY = os.environ.get("django-insecure-cwxn(f*6kos=m1ir$6u8+@ke3ygt@5*fqb)i(b9n(-dtsc_r#9")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [adaptivemathquiz.onrender.com]
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://adaptivemathquiz.onrender.com",
+]
 
 
 # Application definition
@@ -129,6 +135,8 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
